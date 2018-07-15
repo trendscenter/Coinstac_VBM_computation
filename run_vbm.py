04b8daf754abb7966a558ec7fc05289dc14b704a
spanta28 <spanta@mrn.org>
2018-07-15 03:16:02 -0600

    Add files via upload

--- run_vbm.py
+++ run_vbm.py
@@ -5,10 +5,10 @@ This layer includes the interface adapter(IA) for parsing json args to read ni p
 This layer sends the output to vbm_use_cases_layer with the appropriate inputs to run the pipeine using nipype interface
 
 Sample run for bids input data:
-python3 run_vbm.py '{"input":{"opts":{"fwhm": 7}, "BidsDir":"/computation/test_dir/bids_input_data","WriteDir":"/computation/test_dir/"}}'
+python3 run_vbm.py '{"input":{"opts":{"fwhm": 7}, "BidsDir":"/computation/test_dir/bids_input_data","WriteDir":"/computation/test_dir/bids_output"}}'
 
 Sample run for input data of nifti paths in text file:
-python3 run_vbm.py '{"input":{"opts":{"fwhm": 7}, "NiftiPathsTxt":"/computation/test_dir/nifti_paths.txt","WriteDir":"/computation/test_dir/"}}'
+python3 run_vbm.py '{"input":{"opts":{"fwhm": 7}, "NiftiPathsTxt":"/computation/test_dir/nifti_paths.txt","WriteDir":"/computation/test_dir/nifti_outputs"}}'
 """
 
 import ujson as json
@@ -18,29 +18,50 @@ import nibabel as nib
 ## Load Nipype spm interface ##
 from nipype.interfaces import spm
 
-#Import use cases layer and entities layer code
 import vbm_use_cases_layer
 
 #Create a dict to store all paths to softwares,templates & store parameters, names of output files
 
-template_dict={ 'spm_version':'12.7169',
-                'matlab_cmd':'/opt/spm12/run_spm12.sh /opt/mcr/v92 script',
-                'spm_path':'/opt/spm12',
-                'tpm_path':'/opt/spm12/spm12_mcr/spm/spm12/tpm/TPM.nii',
-                'transf_mat_path':'/computation/transform.mat',
-                'scan_type':'T1w',
-                'FWHM_SMOOTH':[6, 6, 6],
-                'BIAS_REGULARISATION':0.0001,
-                'FWHM_GUASSIAN_SMOOTH_BIAS':60,
-                'vbm_output_dirname':'vbm_spm12',
-                'vbm_qc_filename':'vbm_corr_value.txt',
-                'outputs_manual_name':'outputs_description.txt',
-                'outputs_manual_content':"Prefixes descriptions for segmented images:c1-Grey matter,c2-White matter,c3-Cerebro spinal fluid,c4-Bone,c5-Soft tissue,c6-Air(background)"
-                "\nw-Normalized\nm-Modulated\ns-Smoothed with fwhm(mm) [10 10 10]\nFor more info. please refer to spm12 manual here: "
-                "http://www.fil.ion.ucl.ac.uk/spm/doc/manual.pdf and release notes here: http://www.fil.ion.ucl.ac.uk/spm/software/spm12/SPM12_Release_Notes.pdf",
-                'qc_readme_name':'quality_control_readme.txt',
-                'qc_readme_content':"vbm_corr_value.txt gives the correlation value of the swc1*nii file with spm12/tpm/TPM.nii file from SPM12 toolbox"
-                }
+template_dict = {
+    'spm_version':
+    '12.7169',
+    'matlab_cmd':
+    '/opt/spm12/run_spm12.sh /opt/mcr/v92 script',
+    'spm_path':
+    '/opt/spm12/fsroot',
+    'tpm_path':
+    '/opt/spm12/fsroot/spm/spm12/tpm/TPM.nii',
+    'transf_mat_path':
+    os.path.join('/computation', 'transform.mat'),
+    'scan_type':
+    'T1w',
+    'FWHM_SMOOTH': [6, 6, 6],
+    'BIAS_REGULARISATION':
+    0.0001,
+    'FWHM_GAUSSIAN_SMOOTH_BIAS':
+    60,
+    'vbm_output_dirname':
+    'vbm_spm12',
+    'vbm_qc_filename':
+    'vbm_corr_value.txt',
+    'bids_outputs_manual_name':
+    'outputs_description.txt',
+    'nifti_outputs_manual_name':
+    'outputs_description.txt',
+    'bids_outputs_manual_content':
+    "Prefixes descriptions for segmented images:c1-Grey matter,c2-White matter,c3-Cerebro spinal fluid,c4-Bone,c5-Soft tissue,c6-Air(background)"
+    "\nw-Normalized\nm-Modulated\ns-Smoothed with fwhm(mm) [10 10 10]\nFor more info. please refer to spm12 manual here: "
+    "http://www.fil.ion.ucl.ac.uk/spm/doc/manual.pdf and release notes here: http://www.fil.ion.ucl.ac.uk/spm/software/spm12/SPM12_Release_Notes.pdf",
+    'nifti_outputs_manual_content':
+    "sub-1,sub-2,sub-* denotes each nifti file with respect to the order in the nifti paths text file"
+    "Prefixes descriptions for segmented images:c1-Grey matter,c2-White matter,c3-Cerebro spinal fluid,c4-Bone,c5-Soft tissue,c6-Air(background)"
+    "\nw-Normalized\nm-Modulated\ns-Smoothed with fwhm(mm) [10 10 10]\nFor more info. please refer to spm12 manual here: "
+    "http://www.fil.ion.ucl.ac.uk/spm/doc/manual.pdf and release notes here: http://www.fil.ion.ucl.ac.uk/spm/software/spm12/SPM12_Release_Notes.pdf",
+    'qc_readme_name':
+    'quality_control_readme.txt',
+    'qc_readme_content':
+    "In each subject's anat/vbm_spm12 directory,vbm_corr_value.txt gives the correlation value of the swc1*nii file with spm12/tpm/TPM.nii file from SPM12 toolbox"
+}
 """
 More info. on keys in template_dict
 
@@ -108,20 +129,28 @@ def process_bids(args):
     BidsDir = args['input']['BidsDir']
     WriteDir = args['input']['WriteDir']
 
-    if 'opts' in args['input']:opts=args['input']['opts']
-    else:opts=None
+    if 'opts' in args['input']: opts = args['input']['opts']
+    else: opts = None
 
     # Check if input_bids_dir is in BIDS format using bids-validator tool
     cmd = "bids-validator {0}".format(BidsDir)
     bids_process = os.popen(cmd).read()
 
-    # Check if it has T1w data and write permissions to Write dir
-    if bids_process and template_dict['scan_type'] in bids_process and os.access(WriteDir, os.W_OK):
-        return vbm_use_cases_layer.run_bids_pipeline(bids_dir=BidsDir, write_dir=WriteDir,pipeline_opts=opts,**template_dict)
+    # Check if the bids dir has T1w data and write permissions to Write dir
+    if bids_process and template_dict['scan_type'] in bids_process and os.access(
+            WriteDir, os.W_OK):
+        return vbm_use_cases_layer.execute_pipeline(
+            bids_dir=BidsDir,
+            write_dir=WriteDir,
+            data_type='bids',
+            pipeline_opts=opts,
+            **template_dict)
 
     else:
-        sys.stderr.write('Incompatible Bids directory format or write directory does not have permissions')
-
+        sys.stderr.write(
+            'Incompatible Bids directory format or write directory does not have permissions'
+        )
+        return json.dumps({"output": {"success": True}})
 
 
 def process_niftis(args):
@@ -162,47 +191,56 @@ def process_niftis(args):
     paths_file = args['input']['NiftiPathsTxt']
     WriteDir = args['input']['WriteDir']
 
-    if 'opts' in args['input']:opts=args['input']['opts']
-    else:opts=None
+    if 'opts' in args['input']: opts = args['input']['opts']
+    else: opts = None
 
     # Reach each line in nifti_paths.txt into niftis variable
-    niftis=[]
+    niftis = []
     with open(paths_file, "r") as f:
         for line in f:
             niftis.append(line.rstrip(('\n')))
 
     if os.access(WriteDir, os.W_OK):
-        return vbm_use_cases_layer.run_nifti_pipeline(nii_files=niftis, write_dir=WriteDir, pipeline_opts=opts, **template_dict)
+        return vbm_use_cases_layer.execute_pipeline(
+            nii_files=niftis,
+            write_dir=WriteDir,
+            data_type='nifti',
+            pipeline_opts=opts,
+            **template_dict)
     else:
         sys.stderr.write('write directory does not have permissions')
+        return json.dumps({"output": {"success": True}})
+
 
 def software_check():
     """This function returns the spm standalone version installed inside the docker
     """
-    spm.SPMCommand.set_mlab_paths(matlab_cmd=template_dict['matlab_cmd'], use_mcr=True)
-    return(spm.SPMCommand().version)
+    spm.SPMCommand.set_mlab_paths(
+        matlab_cmd=template_dict['matlab_cmd'], use_mcr=True)
+    return (spm.SPMCommand().version)
 
-if __name__=='__main__':
 
-    # Check if spm is running and give 15 secs to connect spm
+if __name__ == '__main__':
+
+    # Check if spm is running
     spm_check = software_check()
-    time.sleep(15)
-    if spm_check is None: 
-        raise ValueError("spm unable to start in vbm docker")
+    if spm_check is None:
+        raise EnvironmentError("spm unable to start in vbm docker")
 
-    # The following block of code assigns the appropriate pre-processing function for input data format, based on Bids or nifti file paths in text file #
-    #args = json.loads(sys.stdin.read()) This is not working for reading in json strings
-
+    # The following block of code assigns the appropriate pre-processing function for input data format, based on Bids or nifti file paths in text file
     args = json.loads(sys.argv[1])
 
-    if args and spm_check==template_dict['spm_version']:
-        if 'BidsDir' in args['input'] and 'WriteDir' in args['input']:
-            computation_output=process_bids(args)
+    if args and (spm_check == template_dict['spm_version']):
+        if ('BidsDir' in args['input']) and ('WriteDir' in args['input']):
+            computation_output = process_bids(args)
             sys.stdout.write(computation_output)
-        elif 'NiftiPathsTxt' in args['input'] and 'WriteDir' in args['input']:
-            computation_output=process_niftis(args)
+        elif ('NiftiPathsTxt' in args['input']) and (
+                'WriteDir' in args['input']):
+            computation_output = process_niftis(args)
             sys.stdout.write(computation_output)
-        else:sys.stderr.write('Cannot read input data from json')
+        else:
+            sys.stderr.write('Cannot read input data from json')
 
     else:
-        raise ValueError("Incompatible json or spm unable to start in vbm docker")
+        raise ValueError(
+            "Incompatible json or spm unable to start in vbm docker")
--- vbm_entities_layer.py
+++ vbm_entities_layer.py
@@ -9,70 +9,48 @@ import nipype.interfaces.spm as spm
 from nipype.interfaces.io import DataSink
 from nipype.interfaces.utility import Function
 
-import nibabel as nb
-import numpy as np
-import math,os
 
 ## 1 Reorientation node & settings ##
 class Reorient:
-	def __init__(self):
-		self.node = pe.Node(interface=spm.ApplyTransform(), name='reorient')
+    def __init__(self, **template_dict):
+        self.node = pe.Node(interface=spm.ApplyTransform(), name='reorient')
+        self.node.inputs.mat = template_dict['transf_mat_path']
+        self.node.inputs.paths = template_dict['spm_path']
+
 
 ## 2 Segementation Node and settings ##
 class Segment:
-	def __init__(self):
-		self.node = pe.Node(interface=spm.NewSegment(), name='segmentation')
+    def __init__(self, **template_dict):
+        self.node = pe.Node(interface=spm.NewSegment(), name='segmentation')
+        self.node.inputs.paths = template_dict['spm_path']
+        self.node.inputs.channel_info = (
+            template_dict['BIAS_REGULARISATION'],
+            template_dict['FWHM_GAUSSIAN_SMOOTH_BIAS'], (False, False))
 
 
 def transform_list(normalized_class_images):
-		return [each[0] for each in normalized_class_images]
+    return [each[0] for each in normalized_class_images]
 
-class List_normalized_images:
-	def __init__(self):
-		self.node=pe.Node(interface=Function(
-			input_names='normalized_class_images',
-			output_names='list_norm_images',
-			function=transform_list
-		), name='List_normalized_images')
+
+class List_Normalized_Images:
+    def __init__(self):
+        self.node = pe.Node(
+            interface=Function(
+                input_names='normalized_class_images',
+                output_names='list_norm_images',
+                function=transform_list),
+            name='List_normalized_images')
+
 
 ## 4 Smoothing Node & Settings ##
 class Smooth:
-	def __init__(self):
-		self.node = pe.Node(interface=spm.Smooth(), name='smoothing')
+    def __init__(self, **template_dict):
+        self.node = pe.Node(interface=spm.Smooth(), name='smoothing')
+        self.node.inputs.paths = template_dict['spm_path']
+        self.node.inputs.fwhm = template_dict['FWHM_SMOOTH']
+
 
 ## 5 Datsink Node that collects segmented, smoothed files and writes to temp_write_dir ##
 class Datasink:
-	def __init__(self):
-		self.node = pe.Node(interface=DataSink(), name='sinker')
-
-#Explore corrcoef
-def get_corr(template,segmented_file,corr_file_name):
-	"""This function computes correlation value of the swc1*nii file with spm12/tpm/TPM.nii file from SPM12 toolbox """
-
-	def get_data(file):
-		a_data = nb.load(file)
-		t_data = a_data.get_data()
-		if len(t_data.shape) == 4:
-			tx, ty, tz, im = t_data.shape
-			st_data = t_data[:, :, :, 0]
-		else:
-			tx, ty, tz = t_data.shape
-			st_data = t_data
-		stn_data = st_data.reshape((tx * ty * tz, 1), order='F')
-		stn_data = np.nan_to_num(stn_data)
-		return stn_data
-
-	cstn_data = get_data(template)
-	cre_data = get_data(segmented_file)
-	indices = np.logical_and(cstn_data!=0,cre_data!=0)
-	fcstn_data, fcre_data = cstn_data[indices], cre_data[indices]
-
-	a=fcstn_data-np.mean(fcstn_data)
-	b=fcre_data-np.mean(fcre_data)
-	covalue = (a * b).sum() / math.sqrt((a * a).sum() * (b * b).sum())
-	write_path= os.path.dirname(segmented_file)
-
-	with open(os.path.join(write_path,corr_file_name),'w') as fp:
-		fp.write("%3.2f\n"%(covalue))
-		fp.close()
-
+    def __init__(self):
+        self.node = pe.Node(interface=DataSink(), name='sinker')
